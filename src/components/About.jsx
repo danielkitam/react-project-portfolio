70e74c8cbd67eeb02f7b8094c66d2aa3ffc11c44
Daniel kitamenywa <danielkitamen@gmail.com>
2023-03-01 16:25:31 +0300

    pushed

---
+++ src/components/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-gradient-to-b 
+    from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+         justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4
+                 border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'>
+                Aliquip eu sunt excepteur amet commodo exercitation duis. Enim sunt ullamco 
+                sit labore quis Lorem elit in nulla nisi velit eiusmod. Anim consectetur 
+                sunt ad nulla eu et elit nisi dolore dolore. Minim aliqua dolor officia 
+                minim veniam dolor consequat non ea in. Et consectetur ipsum fugiat eu id ex ea esse duis est.
+                Aliquip eu sunt excepteur amet commodo exercitation duis. Enim sunt ullamco 
+                sit labore quis Lorem elit in nulla nisi velit eiusmod. Anim consectetur 
+                sunt ad nulla eu et elit nisi dolore dolore. Minim aliqua dolor officia 
+                minim veniam dolor consequat non ea in. Et consectetur ipsum fugiat eu id ex ea esse duis est.
+                Aliquip eu sunt excepteur amet commodo exercitation duis. Enim sunt ullamco 
+                sit labore quis Lorem elit in nulla nisi velit eiusmod. Anim consectetur 
+                sunt ad nulla eu et elit nisi dolore dolore. Minim aliqua dolor officia 
+                minim veniam dolor consequat non ea in. Et consectetur ipsum fugiat eu id ex ea esse duis est.
+            </p>
+
+            <br />
+
+            <p className='text-xl'>
+                Aliquip eu sunt excepteur amet commodo exercitation duis. Enim sunt ullamco 
+                sit labore quis Lorem elit in nulla nisi velit eiusmod. Anim consectetur 
+                sunt ad nulla eu et elit nisi dolore dolore. Minim aliqua dolor officia 
+                minim veniam dolor consequat non ea in. Et consectetur ipsum fugiat eu id ex ea esse duis est.
+                Aliquip eu sunt excepteur amet commodo exercitation duis. Enim sunt ullamco 
+                sit labore quis Lorem elit in nulla nisi velit eiusmod. Anim consectetur 
+                sunt ad nulla eu et elit nisi dolore dolore. Minim aliqua dolor officia 
+                minim veniam dolor consequat non ea in. Et consectetur ipsum fugiat eu id ex ea esse duis est.
+                Aliquip eu sunt excepteur amet commodo exercitation duis. Enim sunt ullamco 
+                sit labore quis Lorem elit in nulla nisi velit eiusmod. Anim consectetur 
+                sunt ad nulla eu et elit nisi dolore dolore. Minim aliqua dolor officia 
+                minim veniam dolor consequat non ea in. Et consectetur ipsum fugiat eu id ex ea esse duis est.
+            </p>
+
+            <br />
+        </div>
+    </div>
+  )
+}
+
+export default About
